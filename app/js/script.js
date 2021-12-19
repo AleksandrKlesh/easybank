c78ff475729c8a144f540a1b2cc7c983080a6541
AleksandrKlesh <63539785+AleksandrKlesh@users.noreply.github.com>
2021-12-20 02:50:18 +0300

    feat: added overlay and animation for menu button

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,12 @@
-console.log("Hello");
+const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+
+btnHamburger.addEventListener("click", function () {
+  console.log("click hamburger");
+
+  if (header.classList.contains("open overlay")) {
+    header.classList.remove("open overlay");
+  } else {
+    header.classList.add("open overlay");
+  }
+});
